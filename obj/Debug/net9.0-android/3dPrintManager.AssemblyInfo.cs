609a4400fde3dcdf0af7b470841549c5584996f1
Melnyk Mykola <kolanio757@gmail.com>
2025-08-26 00:41:21 +0300

    Ignore obj and bin

--- obj/Debug/net9.0-android/3dPrintManager.AssemblyInfo.cs
+++ obj/Debug/net9.0-android/3dPrintManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3dPrintManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+527ec556000be18c1fbda792556c7d0fab753dbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("3dPrintManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3dPrintManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
